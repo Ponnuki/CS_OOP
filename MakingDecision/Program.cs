fa32b6da266140a41b9a39ac94a13b31fc650995
Pornjeds <ponnuki@gmail.com>
2014-06-29 20:49:04 +0700

    initialized

---
+++ MakingDecision/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MakingDecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start...");
+            if (5 < 10)
+            {
+                // Logic true: if statement block
+                Console.WriteLine("Run if statement block");
+            }
+
+            Console.WriteLine("Completed...");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}

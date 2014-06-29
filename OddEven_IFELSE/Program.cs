a522c2eec619036d444ca03c0c7569663f25cdbb
Pornjeds <ponnuki@gmail.com>
2014-06-29 22:07:50 +0700

    Odd Even number example using if else

---
+++ OddEven_IFELSE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OddEven_IFELSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+            int value;
+            string output;
+
+            Console.WriteLine("Please enter a number...");
+            Console.Write("Value:");
+
+            flag = int.TryParse(Console.ReadLine(), out value);
+
+            if (flag == false)
+            {
+                output = "Not a number";
+            }
+
+            if (value % 2 == 1)
+            {
+                output = "Number is odd";
+            }
+            else
+            {
+                output = "Number is even";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}

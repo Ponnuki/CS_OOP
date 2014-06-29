de318be77355fbdcbad92aa05dd1b2a1d3f8643f
Pornjeds <ponnuki@gmail.com>
2014-06-29 22:19:47 +0700

    If else Ternary short hand example

---
+++ OddEven_Ternary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven_Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+            int value;
+            string output;
+
+            Console.WriteLine("Please enter a number...");
+            Console.Write("Value:");
+
+            flag = int.TryParse(Console.ReadLine(), out value);
+
+            if (flag == false)
+            {
+                output = "Not a number";
+            }
+
+            output = (value % 2 == 0) ? "Number is even" : "Number is odd";
+
+            Console.WriteLine(output);
+        }
+    }
+}

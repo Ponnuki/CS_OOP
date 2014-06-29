dffa8d5e3f07a237d1548b3630791b4092c3027a
Pornjeds <ponnuki@gmail.com>
2014-06-29 21:23:52 +0700

    Basic If statement Odd Even console program.

---
+++ OddEven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+            int value;
+            string output = "Number is even";
+
+            Console.WriteLine("Please enter a number...");
+            Console.Write("Value:");
+
+            flag = int.TryParse(Console.ReadLine(), out value);
+
+            if (flag == false)
+            {
+                output = "Not a number";
+            }
+
+            if (value % 2 == 1)
+            {
+                output = "Number is odd";
+            }
+
+            //Show result
+            Console.WriteLine(output);
+        }
+    }
+}

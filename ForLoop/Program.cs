6311b09ddd91f629f0dbd0fb63c148297d185750
Pornjeds <ponnuki@gmail.com>
2014-07-02 23:32:43 +0700

    Add Simple for loop example

---
+++ ForLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For #1");
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("For #2");
+            for (int i = 10 - 1; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("For #3");
+            for (int i = 0; i < 20; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("For #4");
+            for (int i = 10 - 1; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("For #5");
+            for (int i = 0; i < (20 / 2) ; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}

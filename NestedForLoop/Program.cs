d30693497c032eae7bf2befcd480accc06b631ff
Pornjeds <ponnuki@gmail.com>
2014-07-02 23:46:28 +0700

    Add nested for loop example by search for prime number in specific range

---
+++ NestedForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            for (i = 2; i < 100; i++)
+            {
+                for (j = 2; j <= (i / j); j++)
+                    if ((i % j) == 0) break; // if factor found, not prime
+                if (j > (i / j))
+                    Console.WriteLine("{0} is prime", i);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}

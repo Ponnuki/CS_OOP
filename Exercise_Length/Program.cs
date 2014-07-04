116166e21302e1c91c128f012e1a5772e33a067a
Pornjeds <ponnuki@gmail.com>
2014-07-04 14:35:41 +0700

    Add interest compounding example

---
+++ Exercise_Length/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 10;
+            double interest_rate = 0.05;
+            double value = 100;
+
+            Console.WriteLine(string.Format("Start year 0, Asset:{0:C}\n", value));
+
+            for (int i = 1; i <= year; i++)
+            {
+                //Compounding interest
+                value = value * ( 1 + interest_rate);
+                Console.WriteLine("Start of Year {0}, Asset {1:C}", i, value);
+            }
+        }
+    }
+}

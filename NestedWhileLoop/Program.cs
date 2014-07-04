a4360dc31ef49bca3431f0a2775eca1613f22559
Pornjeds <ponnuki@gmail.com>
2014-07-04 12:26:06 +0700

    While loop example statement

---
+++ NestedWhileLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            while (i < 100)
+            {
+                int j = 2;
+
+                while (j <= (i / j))
+                {
+                    if ((i % j) == 0)
+                    {
+                        break;
+                    }
+                    j++;
+                }
+
+                if (j > (i / j))
+                {
+                    Console.WriteLine("{0} is prime number", i);
+                }
+
+                i++;
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}

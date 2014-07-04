b46dd772a27c6bcfe904436bee58f1478135a8ba
Pornjeds <ponnuki@gmail.com>
2014-07-04 13:59:08 +0700

    How to use debugger

---
+++ UseDebugger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UseDebugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 5;
+
+            for (int i = start; i < end; i++)
+            {
+                Console.WriteLine("value: {0}", i);
+            }
+        }
+    }
+}

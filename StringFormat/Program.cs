855eb555464e9475c2ce0cf235bbb612ca0889ac
Pornjeds <ponnuki@gmail.com>
2014-07-22 15:05:36 +0700

    Add string format example

---
+++ StringFormat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "First Name";
+            var b = "Last Name";
+
+            Console.WriteLine("{0} {1}");
+            Console.WriteLine(string.Format("{0} {1}", a, b));
+
+            Console.WriteLine("{0}");
+            Console.WriteLine(string.Format("{0}", a, b));
+        }
+    }
+}
